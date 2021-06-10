dc91f94aa50a3ba13188a89620cb8fa472c2d4d2
LAPTOP-EDEJE2EM\kirki <kirkish.2@gmail.com>
2021-06-09 22:04:24 -0400

    no message

---
+++ Assignment14/Assignment14/src/main/java/com/coderscampus/Assignment14/ChatterBoxApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterBoxApplication.class, args);
+	}
+
+}
